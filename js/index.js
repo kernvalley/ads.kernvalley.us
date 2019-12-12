8e7dc1a8c521bff4a26c379926614651b3138ab5
Chris Zuber <shgysk8zer0@gmail.com>
2019-12-11 17:05:39 -0800

    WIP payment for ads

--- js/index.js
+++ js/index.js
@@ -5,7 +5,89 @@ import 'https://cdn.kernvalley.us/components/current-year.js';
 import 'https://cdn.kernvalley.us/components/gravatar-img.js';
 import 'https://cdn.kernvalley.us/components/login-button.js';
 import 'https://cdn.kernvalley.us/components/logout-button.js';
+import 'https://cdn.kernvalley.us/components/toast-message.js';
+import 'https://cdn.kernvalley.us/components/bacon-ipsum.js';
 import {$, ready, registerServiceWorker} from 'https://cdn.kernvalley.us/js/std-js/functions.js';
+import PaymentRequestShim from 'https://cdn.kernvalley.us/js/PaymentAPI/PaymentRequest.js';
+
+if (! ('PaymentRequest' in window)) {
+	window.PaymentRequest = PaymentRequestShim;
+}
+
+async function pay({views = 500, price = 0.05} = {}) {
+	const terms = document.getElementById('terms');
+	const displayItems = [{
+		label: 'Ad design submission',
+		amount: {
+			currency: 'USD',
+			value: (views * price).toFixed(2),
+		}
+	},];
+	const paymentRequest = new PaymentRequest([{
+		supportedMethods: 'basic-card',
+		data: {
+			supportedNetworks: ['visa', 'mastercard','discover'],
+			supportedTypes: ['credit', 'debit']
+		}
+	}], {
+		displayItems,
+		total: {
+			label: 'Total Cost',
+			amount: {
+				currency: 'USD',
+				value: displayItems.reduce((sum, item) => sum + item.amount.value, 0),
+			}
+		},
+		// shippingOptions: [{
+		// 	id: 'standard',
+		// 	label: 'Standard shipping',
+		// 	amount: {
+		// 		currency: 'USD',
+		// 		value: '0.00'
+		// 	},
+		// 	selected: true
+		// }]
+	}, {
+		requestPayerName: true,
+		requestPayerEmail: true,
+		requestPayerPhone: true,
+		// requestShipping: true,
+	});
+
+	await customElements.whenDefined('toast-message');
+	await terms.show();
+	await terms.closed;
+
+	if (await paymentRequest.canMakePayment()) {
+		try {
+			const paymentResponse = await paymentRequest.show();
+			paymentResponse.complete('success');
+			$('#payment-dialog').remove();
+			console.log(paymentResponse);
+			await customElements.whenDefined('toast-message');
+
+			// const resp = await fetch('http://localhost:8080/payment/', {
+			// 	method: 'POST',
+			// 	headers: new Headers({
+			// 		Accept: 'application/json',
+			// 		'Content-Type': 'application/json'
+			// 	}),
+			// 	mode: 'cors',
+			// 	body: JSON.stringify({paymentResponse, paymentRequest}),
+			// });
+			// const parsed = await resp.json();
+			console.info(paymentResponse);
+		} catch(err) {
+			console.error(err);
+			$('#payment-dialog').remove();
+			await customElements.whenDefined('toast-message');
+			const Toast = customElements.get('toast-message');
+			Toast.toast(err.message);
+		} finally {
+			$('#payment-dialog').remove();
+		}
+	}
+}
 
 if (document.documentElement.dataset.hasOwnProperty('serviceWorker')) {
 	registerServiceWorker(document.documentElement.dataset.serviceWorker).catch(console.error);
@@ -81,6 +163,7 @@ ready().then(async () => {
 
 	$('form[name="ad"]').submit(async event => {
 		event.preventDefault();
+		await pay();
 		const data = new FormData(event.target);
 		console.info({
 			label: data.get('label'),
