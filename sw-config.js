9b1d5685e1cdc785e3c539fed309c7e550cfdc76
Chris Zuber <shgysk8zer0@gmail.com>
2021-01-15 13:53:01 -0800

    Add missing Instagram link

--- sw-config.js
+++ sw-config.js
@@ -30,6 +30,7 @@ const config = {
 		'https://cdn.kernvalley.us/components/ad/block.css',
 		'https://cdn.kernvalley.us/img/keep-kern-clean.svg',
 		'https://cdn.kernvalley.us/img/logos/play-badge.svg',
+		'https://cdn.kernvalley.us/img/logos/instagram.svg',
 		'https://cdn.kernvalley.us/fonts/roboto.woff2',
 	].map(path => new URL(path, location.origin).href),
 	allowed: [
